d150811af2023a4e1ea973ac0287f0e9705f1242
RubenCano <erreceeme20@gmail.com>
2026-01-21 12:21:15 +0100

    definiendo funcnioes

---
+++ persistencia.py
@@ -0,0 +1,10 @@
+def read(rutaArchivo)->dict: 
+    #deserializar
+def update(rutaArchivo, contenido): 
+    
+def write(rutaArchivo, contenido): 
+    #serializar
+def clear(rutaArchivo): 
+    
+def delete(rutaArchivo): 
+    
