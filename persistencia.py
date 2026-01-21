cf8977ed4fdd89538c4a903e433085f13c461b57
RubenCano <erreceeme20@gmail.com>
2026-01-21 13:21:25 +0100

    arreglando

--- persistencia.py
+++ persistencia.py
@@ -2,26 +2,37 @@ import json
 import os
 
 
-def read(rutaArchivo)->dict: 
+def read(rutaArchivo): 
     #deserializar 
     with open(rutaArchivo, "r") as archivo:
-        jsonString=archivo.read()
-        return json.load(jsonString)
+        return json.load(archivo)
+        
 def update(rutaArchivo, contenido): 
     #serializar
     with open(rutaArchivo, "w") as archivo:
-        diccionariosString= json.dump(contenido)
-        archivo.write(diccionariosString)
+        json.dump(contenido, archivo, indent=4 )
+
 def write(rutaArchivo, contenido): 
     #serializar
     with open(rutaArchivo, "a") as archivo:
-        diccionariosString= json.dump(contenido)
-        archivo.write(diccionariosString)
+        json.dump(contenido, archivo, indent=4)
+        
 
 def clear(rutaArchivo): 
     with open(rutaArchivo, "w") as archivo:
         archivo.write("")
 
 def delete(rutaArchivo): 
-    os.
-if __name__="__main__":
+    os.remove(rutaArchivo)
+
+if __name__=="__main__":
+    """write("data/usuarios.json",[{"id": 5,
+  "nombre": "angela",
+  "email": "ana@ejemplo.com"},
+  {"id": 6,
+  "nombre": "alejandra",
+  "email": "ana@ejemplo.com"},
+  {"id": 7,
+  "nombre": "alex",
+  "email": "ana@ejemplo.com"}])"""
+    clear("data/usuarios.json")
