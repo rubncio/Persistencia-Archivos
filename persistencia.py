71609cd85769ece67f52087abec13bc7b046fdcd
RubenCano <erreceeme20@gmail.com>
2026-01-21 12:38:05 +0100

    cambios

--- persistencia.py
+++ persistencia.py
@@ -1,10 +1,27 @@
+import json
+import os
+
+
 def read(rutaArchivo)->dict: 
-    #deserializar
+    #deserializar 
+    with open(rutaArchivo, "r") as archivo:
+        jsonString=archivo.read()
+        return json.load(jsonString)
 def update(rutaArchivo, contenido): 
-    
+    #serializar
+    with open(rutaArchivo, "w") as archivo:
+        diccionariosString= json.dump(contenido)
+        archivo.write(diccionariosString)
 def write(rutaArchivo, contenido): 
     #serializar
+    with open(rutaArchivo, "a") as archivo:
+        diccionariosString= json.dump(contenido)
+        archivo.write(diccionariosString)
+
 def clear(rutaArchivo): 
-    
+    with open(rutaArchivo, "w") as archivo:
+        archivo.write("")
+
 def delete(rutaArchivo): 
-    
+    os.
+if __name__="__main__":
