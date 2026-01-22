261d45745b4249c9eb0159fea5dc24f24c002723
RubenCano <erreceeme20@gmail.com>
2026-01-22 12:08:35 +0100

    cambio de acceso en metodos

--- persistencia.py
+++ persistencia.py
@@ -9,12 +9,12 @@ def read(rutaArchivo):
         
 def update(rutaArchivo, contenido): 
     #serializar
-    with open(rutaArchivo, "w") as archivo:
+    with open(rutaArchivo, "a") as archivo:
         json.dump(contenido, archivo, indent=4 )
 
 def write(rutaArchivo, contenido): 
     #serializar
-    with open(rutaArchivo, "a") as archivo:
+    with open(rutaArchivo, "w") as archivo:
         json.dump(contenido, archivo, indent=4)
         
 
@@ -26,7 +26,7 @@ def delete(rutaArchivo):
     os.remove(rutaArchivo)
 
 if __name__=="__main__":
-    """write("data/usuarios.json",[{"id": 5,
+    write("data/usuarios.json",[{"id": 5,
   "nombre": "angela",
   "email": "ana@ejemplo.com"},
   {"id": 6,
@@ -34,5 +34,4 @@ if __name__=="__main__":
   "email": "ana@ejemplo.com"},
   {"id": 7,
   "nombre": "alex",
-  "email": "ana@ejemplo.com"}])"""
-    clear("data/usuarios.json")
+  "email": "ana@ejemplo.com"}])
