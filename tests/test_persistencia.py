e4aa577339ee5a0a9d962b391c8e8cdaa7d46a94
RubenCano <erreceeme20@gmail.com>
2026-01-22 12:28:43 +0100

    definicion inicial de test

---
+++ tests/test_persistencia.py
@@ -0,0 +1,59 @@
+def readRutaInvalida():
+    #Entrada:ruta de fichero inexistente
+    #Salida:error
+    pass
+def readficheroVacio():
+    #Entrada:ruta de fichero sin contenido
+    #Salida:objeto vacio
+    pass
+def readExitosa():
+    #Entrada:ruta de fichero con contenido
+    #Salida:objeto correspondiente al contenido del fichero
+    pass
+
+def updateExitoso():
+    #Entrada:Ruta valida y contenido.
+    #Salida:OK
+    pass
+def updateContenidoVacio():
+    #Entrada:parametro Contenido vacio
+    #Salida:OK
+    pass
+def updateRutaInvalida():
+    #Entrada:ruta de fichero inexistente
+    #Salida:error
+    pass
+
+def writeExitoso():
+    #Entrada:ruta valida
+    #Salida:fichero con el contenido indicado en el parametro
+    pass
+def writeContenidoVacio():
+    #Entrada:parametro Contenido vacio
+    #Salida:OK
+    pass
+
+def writeRutaInvalida():
+    #Entrada:ruta de fichero inexistente
+    #Salida:error
+    pass
+
+def clearExitoso():
+    #Entrada:Ruta de fichero valida
+    #Salida:fichero vacio
+    pass
+
+def clearRutaInvalida():
+    #Entrada:ruta de fichero inexistente
+    #Salida:error
+    pass
+
+def deleteExitoso():
+    #Entrada:ruta válida
+    #Salida:fichero borrado
+    pass
+
+def deleteRutaInvalida():
+    #Entrada:ruta de fichero inexistente
+    #Salida:error
+    pass
